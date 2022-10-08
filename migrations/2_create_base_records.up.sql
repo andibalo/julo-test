5991f9ba1855ac1ab303e41733386f49676e554f
t-andi.usman <t-andi.usman@ovo.id>
2022-10-08 18:08:13 +0700

    refactoring and add postman collection

--- migrations/2_create_base_records.up.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO cake (title,description,rating,image) VALUES
-  ("73c30425-1af5-4095-bf88-4cf21841dfd5","Cheesecake","Best cheesecake in the world", 9.2, "http://cakeimage.com"),
-  ("23c30425-13f5-4295-df88-4cf21841dfd5","Mooncake","Best mooncake in the world", 8.2, "http://cakeimage.com"),
-  ("d3c30425-1335-1095-ce38-3cf21841dfd5","Kue Lapis","Best kue lapis in the world", 9.8, "http://cakeimage.com"),
-  ("e3c30425-a335-1295-ce38-3cf21e41dfd5","Red velvet","Best red velvet in the world", 8.4, "http://cakeimage.com"),
-  ("77e6db98-12c5-4d4f-bebb-f92ef686f1ea","Birthday Cake","Best birthday cake in the world", 7.8, "http://cakeimage.com"),
-  ("036d041a-d998-4492-9894-b5bd59a348d3","Sweet cake","Best sweet cake in the world", 8.8, "http://cakeimage.com"),
-  ("421db05a-5edc-4c43-bd31-ae0744501888","Chocolate cake","Best chocolate cake in the world", 7.5, "http://cakeimage.com");
